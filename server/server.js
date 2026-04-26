d2eff6961a95759241720cbd99114b026b921fd1
Paul Harold <149643053+paulharoldb@users.noreply.github.com>
2026-04-26 23:13:04 +0800

    fixed cors

--- server/server.js
+++ server/server.js
@@ -11,7 +11,13 @@ dns.setServers(["1.1.1.1", "8.8.8.8"]);
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://anivie.vercel.app"
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 // ==========================================
